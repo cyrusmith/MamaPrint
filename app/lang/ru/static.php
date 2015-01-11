6b29653daa6877605866265946c110f9c8dae8ce
cyrusmith <info@interosite.ru>
2015-01-11 19:37:14 +0500

    + save form + migration

--- app/lang/ru/static.php
+++ app/lang/ru/static.php
@@ -1,9 +1,26 @@
 <?php
 return [
+    'admin.save' => 'Сохранить',
     'admin.catitem.title' => 'Название',
     'admin.catitem.title.help' => 'Заголовок товара',
     'admin.catitem.breaf' => 'Короткое описание',
     'admin.catitem.breaf.help' => 'Отображается в списке товаров',
     'admin.catitem.descr' => 'Описание',
-    'admin.pagetitle.catalog' => 'Каталог'
+    'admin.pagetitle.catalog' => 'Каталог',
+    'admin.catitem.slug' => 'Транскрипция',
+    'admin.catitem.slug.help' => 'Используется в url',
+    'admin.catitem.tags' => 'Теги',
+    'admin.catitem.tags.help' => 'Теги через запятую',
+    'admin.catitem.priсe' => 'Цена',
+    'admin.catitem.priсe.help' => 'Основная цена товара',
+    'admin.catitem.priceold' => 'Старая цена',
+    'admin.catitem.priceold.help' => 'Зачеркнутая цена',
+    'admin.catitem.priceregistered' => 'Цена для залогиненых',
+    'admin.catitem.priceregistered.help' => 'Цена для залогиненых',
+    'admin.catitem.age' => 'Возраст',
+    'admin.catitem.age.help' => 'Возраст',
+    'admin.catitem.targets' => 'Что развиваем',
+    'admin.catitem.targets.help' => 'Что развиваем',
+    'admin.catitem.level' => 'Уровень',
+    'admin.catitem.level.help' => 'Легкий, средний, стожный, и т.п.',
 ];
--- app/views/admin/catalog/save.blade.php
+++ app/views/admin/catalog/save.blade.php
@@ -1,28 +1,85 @@
 @extends('layouts.admin')
 
 @section('pagetitle')
-   Каталог|Новый товар
+    Каталог|Новый товар
 @stop
 
 @section('content')
     <form action="" method="post">
         <div class="form-group">
             <label for="catitemtitle">{{Lang::get('static.admin.catitem.title')}}</label>
-            <input type="text" class="form-control" id="catitemtitle" placeholder="{{Lang::get('static.admin.catitem.title.help')}}">
+            <input type="text" class="form-control" id="catitemtitle"
+                   placeholder="{{Lang::get('static.admin.catitem.title.help')}}">
+        </div>
+        <div class="form-group">
+            <label for="catitemslug">{{Lang::get('static.admin.catitem.slug')}}</label>
+            <input type="text" class="form-control" id="catitemslug"
+                   placeholder="{{Lang::get('static.admin.catitem.slug.help')}}">
         </div>
         <div class="form-group">
             <label for="catitembreaf">{{Lang::get('static.admin.catitem.breaf')}}</label>
-            <input type="text" class="form-control" id="catitembreaf" placeholder="{{Lang::get('static.admin.catitem.breaf.help')}}">
+            <input type="text" class="form-control" id="catitembreaf"
+                   placeholder="{{Lang::get('static.admin.catitem.breaf.help')}}">
         </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label for="catitempriсe">{{Lang::get('static.admin.catitem.priсe')}}</label>
+                    <input type="text" class="form-control" id="catitempriсe"
+                           placeholder="{{Lang::get('static.admin.catitem.priсe.help')}}">
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label for="catitempriceold">{{Lang::get('static.admin.catitem.priceold')}}</label>
+                    <input type="text" class="form-control" id="catitempriceold"
+                           placeholder="{{Lang::get('static.admin.catitem.priceold.help')}}">
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label for="catitempriceregistered">{{Lang::get('static.admin.catitem.priceregistered')}}</label>
+                    <input type="text" class="form-control" id="catitempriceregistered"
+                           placeholder="{{Lang::get('static.admin.catitem.priceregistered.help')}}">
+                </div>
+            </div>
+
+        </div>
+
         <div class="form-group">
             <label for="article">{{Lang::get('static.admin.catitem.descr')}}</label>
             <textarea class="wysiwyg"></textarea>
         </div>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox"> Check me out
-            </label>
+        <div class="form-group">
+            <label for="catitemtags">{{Lang::get('static.admin.catitem.tags')}}</label>
+            <input type="text" class="form-control" id="catitemtags"
+                   placeholder="{{Lang::get('static.admin.catitem.tags.help')}}">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+
+        <fieldset>
+            <legend>Доп. информация</legend>
+            <div class="form-group">
+                <label for="catitemage">{{Lang::get('static.admin.catitem.age')}}</label>
+                <input type="text" class="form-control" id="catitemage"
+                       placeholder="{{Lang::get('static.admin.catitem.age.help')}}">
+            </div>
+
+            <div class="form-group">
+                <label for="catitemtargets">{{Lang::get('static.admin.catitem.targets')}}</label>
+                <input type="text" class="form-control" id="catitemtargets"
+                       placeholder="{{Lang::get('static.admin.catitem.targets.help')}}">
+            </div>
+
+            <div class="form-group">
+                <label for="catitemlevel">{{Lang::get('static.admin.catitem.level')}}</label>
+                <input type="text" class="form-control" id="catitemlevel"
+                       placeholder="{{Lang::get('static.admin.catitem.level.help')}}">
+            </div>
+
+        </fieldset>
+        <button type="submit" class="btn btn-default">{{Lang::get('static.admin.save')}}</button>
     </form>
 @stop
